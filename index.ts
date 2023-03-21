897b304a26271d78547ef4669c98d06b7c8c9a29
Sandeep Ramgolam <sandeep@ramgolam.com>
2023-03-21 13:42:16 +0400

    check error + output stream

--- index.ts
+++ index.ts
@@ -8,4 +8,16 @@ window.addEventListener('load', async () => {
     output('Booting...');
     const wc = await WebContainer.boot()
     output('Booted!');
+
+    const process = await wc.spawn('node', ['--version']);
+
+    process.output.pipeTo(new WritableStream({
+        write: (chunk) => {
+            output('Process output: ' + chunk);
+        }
+    }));
+
+    if (await process.exit) {
+        output('Exited with code: ' + process.exit);
+    }
 })

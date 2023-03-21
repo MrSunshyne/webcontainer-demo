a4738424a776e53a40a9e07b2f4b7461802cb471
Sandeep Ramgolam <sandeep@ramgolam.com>
2023-03-21 11:32:08 +0400

    install webcontainer + boot + log

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log('hello frontendmurs')
+import { WebContainer } from '@webcontainer/api';
+
+function output(text) {
+    console.log(text);
+}
+
+window.addEventListener('load', async () => {
+    output('Booting...');
+    const wc = await WebContainer.boot()
+    output('Booted!');
+})
